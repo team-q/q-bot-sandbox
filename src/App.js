1af683dfe7ae5874c667e16e55a27d1d6bdf077d
Kristin Hortsch <kristinhortsch@gmail.com>
2019-03-22 16:03:47 -0700

    we're back

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@ import React, { Component } from 'react';
 import QuestionForm from './components/QuestionForm';
 import { addQuestion } from './actions/questions';
 import { ConnectQuestions } from './components/Questions';
+import { subscribe } from './services/firebase';
 
 class App extends Component {
+
+  componentDidMount() {
+    subscribe(user => {
+      console.log(user)
+    })
+  }
 
   handleSubmit = (name, question, event) => {
     event.preventDefault();
