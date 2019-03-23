5233169d928baaf04241f1826ab29546ab4d8b1c
Kristin Hortsch <kristinhortsch@gmail.com>
2019-03-23 12:41:26 -0700

    fixed handle submit place so form resets

--- src/components/QuestionDatabase.js
+++ src/components/QuestionDatabase.js
@@ -1,13 +1,9 @@
 import React, { PureComponent } from 'react';
 import { ConnectQuestions } from './Questions';
 import QuestionForm from './QuestionForm';
-import { addQuestion } from '../actions/questions';
 
 export default class QuestionDatabase extends PureComponent {
-  handleSubmit = (name, question, event) => {
-    event.preventDefault();
-    addQuestion({ name, question })
-  }
+ 
   
   render() {
     return (
