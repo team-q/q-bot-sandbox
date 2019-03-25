c5c99e507735ce532f2e3fa84f308a2fae6d8864
Cari Palazzolo <cari.pizza@gmail.com>
2019-03-25 15:13:42 -0700

    helloSlack posts to firestore channel collection, removes dupes!

--- functions/index.js
+++ functions/index.js
@@ -1,22 +1,50 @@
+require('dotenv').config();
 const functions = require('firebase-functions');
+const req = require('superagent');
 
 const admin = require('firebase-admin');
 admin.initializeApp();
 
+exports.processQuestion = functions.firestore.document('channel/{id}').onCreate(async(snap, context) => {
+  const { slackId, messageId } = snap.data();
+  console.log('snap.data(): ', snap.data());
+  const matches = await admin.firestore().collection('channel').where('messageId', '==', messageId).get()
+  if(matches.docs.length > 1) return admin.firestore().collection('channel').doc(context.params.id).delete();
+  return req.get(`https://slack.com/api/users.info?token=${process.env.TOKEN}&user=${slackId}&pretty=1`)
+    .then(res => {
+      console.log('line 16, slack get request "res.body:" ', res.body);
+      return admin.firestore().collection('channel').doc(context.params.id).update({ name: res.body.user.real_name })
+    })
+})
+
 exports.helloSlack = functions.https.onRequest((request, response) => {
   // if (request) {
-    // console.log('request.body: ', request.body);
+    console.log('request.body: ', request.body);
     // response.status(200).send(request.body);
 
-    return admin.firestore().collection('channel').add({ name: request.body.event.user, question: request.body.event.text })
-      .then(user => {
-        return request.get('https://slack.com/api/users.info?token=xoxp-579541968176-583390193862-577364688145-c5955328b628f04f9825ef4105db7ffb&user=UH5BG5PRC&pretty=1')
-          .then(results => {
-            // const { id } = user.id;
-            const id = '7E7EcfcvAcCnzMa9Poqk';
-            return admin.firestore().collection('channel').doc(id).update({ ...user, name: results.real_name })
-          })
-      })
+    return admin.firestore().collection('channel').add({ messageId: request.body.event.client_msg_id, name: '', slackId: request.body.event.user, question: request.body.event.text })
+      .then(() => response.status(200).send(request.body));
+      // .then(async(snap) => {
+      //   // console.log('line 13, user._path: ', user._path.segments[1])
+
+      //   const userSnap = await snap.get();
+      //   console.log('line 17 snap: ', snap);
+      //   const userObj = userSnap.data(); // name
+      //   const docID = userSnap.id; // doc id
+
+      //   // const docId = snap._path.segments[1];
+
+      //   return req.get('https://slack.com/api/users.info?token=xoxp-579541968176-583390193862-584929040352-c60bce2126fa813107661000a0fcbd85&user=UH5BG5PRC&pretty=1')
+      //   // return request.get('https://yesno.wtf/api', {
+      //   //   headers: {
+      //   //     'Content-Type': 'application/json'
+      //   //   }
+      //   // })
+      //     .then(async(results) => {
+      //       await console.log('line 16, slack get request "results:" ', results);
+      //       // return admin.firestore().collection('channel').doc(id).update({ ...user, name: results.real_name })
+      //     })
+      // })
 
     // return admin.database().ref('/slack').push({ body: request.body });
   // } else {
