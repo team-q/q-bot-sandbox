28ab7b4d200a851ba99752073eebb5ba70160a59
Kristin Hortsch <kristinhortsch@gmail.com>
2019-03-26 11:50:42 -0700

    the response is alright, commit before i break things

--- functions/index.js
+++ functions/index.js
@@ -17,18 +17,15 @@ exports.processQuestion = functions.firestore.document('channel/{id}').onCreate(
     })
 })
 
-exports.helloSlack = functions.https.onRequest((request, response) => {
-  // if (request) {
+exports.helloSlack = functions.https.onRequest(async(request, response) => {
     console.log('request.body: ', request.body);
-    // response.status(200).send(request.body);
-
+    
     return req
       .post('https://hooks.slack.com/services/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')
       .set('Content-Type', 'application/json')
       .send({ text: 'your question has been added to the queue', thread_ts: request.body.event.ts })
-      .then((message) => {
-        console.log('message', message);
-        return admin.firestore().collection('channel').add({ messageId: request.body.event.client_msg_id, name: '', TA: '', slackId: request.body.event.user, question: request.body.event.text })
+      .then(() => {
+        return admin.firestore().collection('channel').add({ messageId: request.body.event.client_msg_id, name: '', slackId: request.body.event.user, question: request.body.event.text })
           .then(() => response.status(200).send(request.body))
       })
 });
