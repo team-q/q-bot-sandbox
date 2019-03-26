d0876063b7e36adaa73a5ec187c0bc4093d6ef58
Kristin Hortsch <kristinhortsch@gmail.com>
2019-03-26 12:03:40 -0700

    working Q response, but still responding with duplicate messages if its not warm

--- functions/index.js
+++ functions/index.js
@@ -18,14 +18,13 @@ exports.processQuestion = functions.firestore.document('channel/{id}').onCreate(
 })
 
 exports.helloSlack = functions.https.onRequest(async(request, response) => {
-    console.log('request.body: ', request.body);
-    
-    return req
-      .post('https://hooks.slack.com/services/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')
-      .set('Content-Type', 'application/json')
-      .send({ text: 'your question has been added to the queue', thread_ts: request.body.event.ts })
-      .then(() => {
-        return admin.firestore().collection('channel').add({ messageId: request.body.event.client_msg_id, name: '', slackId: request.body.event.user, question: request.body.event.text })
-          .then(() => response.status(200).send(request.body))
-      })
+  console.log('request.body: ', request.body);
+  return req
+  .post('https://hooks.slack.com/services/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')
+  .set('Content-Type', 'application/json')
+  .send({ text: 'your question has been added to the queue', thread_ts: request.body.event.ts })
+  .then(() => {
+    return admin.firestore().collection('channel').add({ messageId: request.body.event.client_msg_id, name: '', slackId: request.body.event.user, question: request.body.event.text })
+      .then(() => response.status(200).send(request.body))
+  })
 });
