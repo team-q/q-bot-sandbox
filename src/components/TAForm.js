bd360bbe4f82437a3d5b7514429aacf8759473e3
Kristin Hortsch <kristinhortsch@gmail.com>
2019-03-26 15:54:17 -0700

    form working after switching to react hooks

--- src/components/TAForm.js
+++ src/components/TAForm.js
@@ -1,31 +1,21 @@
-import React, { PureComponent } from 'react';
+import React, { useState } from 'react';
 
-export default class TAForm extends PureComponent {
-  state = {
-    name: '',
-    cohort: ''
-  }
+export default function TAForm({ handleSubmit }) {
+  const [ name, setName ] = useState('')
+  const [ cohort, setCohort ] = useState('')
 
-  handleChange = ({ target }) => {
-    this.setState({ [target.name]: target.value })
-    console.log(this.state.cohort);
-  }
-
-  render() {
-    const { name, cohort } = this.state;
     return (
       <>
         <h2>Add New TA</h2>
-        <form onSubmit={this.props.handleSubmit.bind(null, name, cohort)}>
+        <form onSubmit={handleSubmit.bind(null, name, cohort)}>
           <label>TA Name</label>
-          <input type='text' name='name' value={name} onChange={this.handleChange}/>
+          <input type='text' name='name' value={name} onChange={({target}) => setName(target.value)}/>
   
-          <label>Assigned Class</label>
-          <input type='text' name='cohort' value={cohort} onChange={this.handleChange}/>
+          <label>Assigned Cohort</label>
+          <input type='text' name='cohort' value={cohort} onChange={({target}) => setCohort(target.value)}/>
 
           <button type='submit'>Submit</button>
         </form>
       </>
     );
-  }
 }
