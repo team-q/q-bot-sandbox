d2763673b4664b1ec3636b6061285843073cc06a
Kristin Hortsch <kristinhortsch@gmail.com>
2019-03-26 13:12:05 -0700

    fixed threading stuff

--- functions/index.js
+++ functions/index.js
@@ -14,19 +14,20 @@ exports.processQuestion = functions.firestore.document('channel/{id}').onCreate(
   return req.get(`https://slack.com/api/users.info?token=${process.env.TOKEN}&user=${slackId}&pretty=1`)
     .then(res => {
       console.log('line 16, slack get request "res.body:" ', res.body);
-
       return admin.firestore().collection('channel').doc(context.params.id).update({ name: res.body.user.real_name })
+    })
+    .then(() => {
+      // const matches = await admin.firestore().collection('channel').where('messageId', '==', messageId).get()
+      // if(matches.docs.length > 2) return admin.firestore().collection('channel').doc(context.params.id).delete();
+      return req
+        .post('https://hooks.slack.com/services/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')
+        .set('Content-Type', 'application/json')
+        .send({ text: 'your question has been added to the queue', thread_ts: snap.data().timestamp })
     })
 });
 
 exports.helloSlack = functions.https.onRequest((request, response) => {
   console.log('request.body: ', request.body);
-
-  return req
-    .post('https://hooks.slack.com/services/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')
-    .set('Content-Type', 'application/json')
-    .send({ text: 'your question has been added to the queue', thread_ts: request.body.event.ts })
-    .then(() => {
       return admin.firestore().collection('channel').add({
         messageId: request.body.event.client_msg_id,
         name: '',
@@ -35,5 +36,4 @@ exports.helloSlack = functions.https.onRequest((request, response) => {
         timestamp: request.body.event.ts
       })
         .then(() => response.status(200).send(request.body));
-    })
 });
