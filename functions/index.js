daaf736e0425bacd71b7f1070b76fc6aa9b3eec1
Kristin Hortsch <kristinhortsch@gmail.com>
2019-03-26 09:23:35 -0700

    index stuff

--- functions/index.js
+++ functions/index.js
@@ -24,16 +24,19 @@ exports.helloSlack = functions.https.onRequest((request, response) => {
     console.log('request.body: ', request.body);
     // response.status(200).send(request.body);
 
-    return admin.firestore().collection('channel').add({ messageId: request.body.event.client_msg_id, name: '', TA: '', slackId: request.body.event.user, question: request.body.event.text })
-      .then(() => response.status(200).send(request.body))
-      // .then(() => {
-      //   console.log('line 18, hit request')
-      //   return req
-      //     .post('https://hooks.slack.com/services/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')
-      //     .set('Content-Type', 'application/json')
-      //     .send({ text: 'hello w' })
-      //     .then(res => console.log)
-      // })
+    return req
+          .post('https://hooks.slack.com/services/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')
+          .set('Content-Type', 'application/json')
+          .send({ text: 'hello w' })
+          .then((res) => {
+            console.log('res', res.body)
+            return admin.firestore().collection('channel').add({ messageId: request.body.event.client_msg_id, name: '', TA: '', slackId: request.body.event.user, question: request.body.event.text })
+              .then(() => response.status(200).send(request.body))
+              .then(() => {
+                console.log('line 18, hit request')
+              })
+          })
+
       // .then(async(snap) => {
       //   // console.log('line 13, user._path: ', user._path.segments[1])
 
