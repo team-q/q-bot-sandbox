083e0c79bddd74a7738bd786655942177d3f66a1
Aaron Dennis <AMDennis1987@gmail.com>
2019-03-27 14:02:40 -0700

    added label to filter

--- src/components/FilterForm.js
+++ src/components/FilterForm.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 export default function FilterForm({ value, onChange }) {
   return (
-    <input type="text" name="searchTerm" value={value} onChange={onChange} />
+    <>
+      <label>Filter by Question:</label>
+      <input type="text" name="searchTerm" value={value} onChange={onChange} />
+    </>
   )
 }
