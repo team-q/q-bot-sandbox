5789920bc76c23063f697e699b75178c2f7b8636
Kristin Hortsch <kristinhortsch@gmail.com>
2019-03-28 09:12:04 -0700

    random Q BOT WORKINGGGG (i think)

--- functions/index.js
+++ functions/index.js
@@ -18,15 +18,21 @@ exports.processQuestion = functions.firestore.document('question/{id}').onCreate
       .then(() => {
         return req.get(`https://slack.com/api/channels.info?token=${process.env.CHANNEL_TOKEN}&channel=${channelId}&pretty=1`)
           .then((res) => {
-            return admin.firestore().collection('question').doc(context.params.id).update({ channelName: res.body.channel.name })
+            admin.firestore().collection('question').doc(context.params.id).update({ channelName: res.body.channel.name })
+            if(res.body.channel.name === 'general') {
+              return req
+                .post('https://hooks.slack.com/services/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')
+                .set('Content-Type', 'application/json')
+                .send({ text: 'your question has been added to the queue', thread_ts: snap.data().timestamp })
+            }
+            else if(res.body.channel.name === 'random') {
+              return req
+                .post('https://hooks.slack.com/services/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')
+                .set('Content-Type', 'application/json')
+                .send({ text: 'your question has been added to the queue', thread_ts: snap.data().timestamp })
+            }
           })
         })
-    })
-    .then(() => {
-      return req
-        .post('https://hooks.slack.com/services/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')
-        .set('Content-Type', 'application/json')
-        .send({ text: 'your question has been added to the queue', thread_ts: snap.data().timestamp })
     })
 });
 
