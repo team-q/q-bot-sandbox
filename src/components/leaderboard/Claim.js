e645e05110f63911567dcc7a990aee61b9d9502a
Kristin Hortsch <kristinhortsch@gmail.com>
2019-03-28 14:34:48 -0700

    we can see claims in the database

--- src/components/leaderboard/Claim.js
+++ src/components/leaderboard/Claim.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { useFirestore } from '../connectFirestore';
 import { questionCollection } from '../../services/firebase';
+import { updateClaim } from '../../actions/TA';
 
-export default function Claim({ name }) {
+export default function Claim({ name, id }) {
   const claims = useFirestore(questionCollection.where('TA', '==', name))
   if(!claims) return <p>Looking up claims</p>
-  
-  return (
-    <h1>{claims.length}</h1>
-  )
+  else {
+    const claimsLength = claims.length;
+    updateClaim(claimsLength, id)
+    return (
+      <h1>{claims.length}</h1>
+    )
+  }
 }
--- src/components/leaderboard/TA.js
+++ src/components/leaderboard/TA.js
@@ -6,7 +6,7 @@ export default function TA({ name, cohort, id, claims, handleDelete}) {
     <tr className={'tableRow'}>
       <td className={'tableData'}>{name || 'User'}</td>
       <td className={'tableData'}>{cohort || '(No cohort provided)'}</td>
-      <td className={'tableData'}>{name && <Claim name={name}/>}</td>
+      <td className={'tableData'}>{name && <Claim id={id} name={name}/>}</td>
       <td className={'tableData'}><button className={'taDeleteButton'}onClick={handleDelete.bind(null, id)}>X</button></td>
     </tr>
   )
