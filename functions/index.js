d4e4bef89d3442166b1dbb5031c7bf209826aa85
Kristin Hortsch <kristinhortsch@gmail.com>
2019-03-27 17:19:13 -0700

    whoops

--- functions/index.js
+++ functions/index.js
@@ -36,24 +36,14 @@ exports.saveChannel = functions.firestore.document('question/{id}').onCreate(asy
   const matches = await admin.firestore().collection('cohort').where('channelId', '==', channelId).get()
   if(!matches.docs.length || matches.docs.length === 0) {
     return admin.firestore().collection('cohort').add({ channelId })
-    // .then(() => {
-    //     return req.get(`https://slack.com/api/channels.info?token=xoxb-585473971876-587835900981-asCQX1py9fSSXW25VXQMWPsZ&channel=${channelId}&pretty=1`)
-    //       .then((res) => {
-    //         console.log(res.body.channel.id);
-    //         return admin.firestore().collection('cohort').where('channelId', '==', res.body.channel.id).get()
-    //           .then((matches) => {
-    //             console.log('DOCS', matches.docs); // snap doesnt work 
-    //             return admin.firestore().collection('cohort').doc(matches.docs[0].id).update({ channelName: res.body.channel.name })
-    //           })
-    //       })
-    //     })
+
       }
     })
 
 exports.saveCohortName = functions.firestore.document('cohort/{id}').onCreate(async(snap, context) => {
   const { channelId } = snap.data()
   const id = context.params.id;
-  return req.get(`https://slack.com/api/channels.info?token=xoxb-585473971876-587835900981-asCQX1py9fSSXW25VXQMWPsZ&channel=${channelId}&pretty=1`)
+  return req.get(`https://slack.com/api/channels.info?token=${process.env.CHANNEL_TOKEN}Z&channel=${channelId}&pretty=1`)
     .then((res) => {
       const channel = res.body.channel.name;
       return admin.firestore().collection('cohort').doc(id).update({ channelName: channel })
