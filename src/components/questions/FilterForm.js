399de262cd920c4e51ce48ef4c2b31df61092f17
Cari Palazzolo <cari.pizza@gmail.com>
2019-04-19 16:31:57 -0700

    use Filter & Sort modal in Questions component

--- src/components/questions/FilterForm.js
+++ src/components/questions/FilterForm.js
@@ -3,9 +3,9 @@ import React from 'react';
 export default function FilterForm({ value, onChange }) {
   return (
     <>
-      <h2 className={'addFilterHeader'}>Filter & Sort </h2>
-      <label className={'questionLabel'}>Question:</label>
-      <input type="text" name="searchTerm" value={value} onChange={onChange} />
+      <label className={'questionLabel'}>By Question:
+        <input type="text" name="searchTerm" value={value} onChange={onChange} />
+      </label>
     </>
   )
 }
